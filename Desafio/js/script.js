e57bf072ddb097499770919841f3d7226610cd70
Tiago Abudi <tiagoabudi96@gmail.com>
2020-12-14 11:26:48 -0300

    desafio

--- Desafio/js/script.js
+++ Desafio/js/script.js
@@ -23,9 +23,70 @@ async function fetchUsers() {
     'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
   );
   const json = await res.json();
+  users = json.results
+    .map(({ login, name, dob, gender, picture }) => {
+      const fullName = `${name.first} ${name.last}`;
+      return {
+        id: login.uuid,
+        name: fullName,
+        nameLowerCase: fullName.toLowerCase(),
+        age: dob.age,
+        gender: gender,
+        picture: picture.large,
+      };
+    })
+    .sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
 }
 
-function addEvents(){
-  inputSearch.addEventListener('keyup', handleKeyUP)
+function addEvents() {
+  inputSearch.addEventListener('keyup', handleKeyUP);
 }
 
+function handleKeyUP(event) {
+  const currentKey = event.key;
+  if (currentKey !== 'Enter') {
+    return;
+  }
+  const filterText = event.target.value;
+  if (filterText.trim() !== '') {
+    filterUsers(filterText);
+  }
+}
+
+function filterUsers(filterText) {
+  const filterTextLowerCase = filterText.toLowerCase();
+
+  const filteredUsers = users.filter((user) => {
+    return user.nameLowerCase.includes(filterTextLowerCase);
+  });
+
+  renderUsers(filteredUsers);
+  renderStatistics(filtered)
+}
+
+function renderUsers(users) {
+  panelUsers.innerHTML = ' ';
+
+  const h2 = document.createElement('h2');
+  h2.textContent = `${users.length} usuário(s) encontrado(s)`;
+
+  const ul = document.createElement('ul');
+
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.classList.add('flex-row');
+    li.classList.add('space-bottom');
+
+    const img = `<img class="avatar" src="${user.picture}" alt="${user.name}"/>`;
+    const userData = `<span>${user.name}, ${user.age} anos</span>`;
+
+    li.innerHTML = `${img}${userData}`;
+
+    ul.appendChild(li);
+  });
+
+  panelUsers.appendChild(h2);
+  panelUsers.appendChild(ul);
+}
